1988df61d53e291a8feb51e6b0df5f61ee1fdf1f
Martin Bruno <martinbruno.mb@outlook.com>
2021-04-05 12:39:23 -0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require ('express');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+
+const knex = require('knex')
+
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0
+
+const register = require ('./controllers/register');
+const signin = require ('./controllers/signin');
+const profile = require ('./controllers/profile');
+const image = require ('./controllers/image');
+
+const db = knex({
+    client: 'pg',
+    connection: {
+      connectionString: process.env.DATABASE_URL,
+      ssl: true
+    }
+  });
+
+const app = express();
+
+app.use (express.json());
+
+app.use(cors())
+
+app.get('/', (req,res) => {res.send('it is working')})
+
+app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt) })
+
+app.post('/register', (req, res) => { register.handleRegister (req, res, db, bcrypt) })
+
+app.get('/profile/:id', (req, res) => { profile.handleProfileGet (req, res, db)  }) 
+
+app.put('/image', (req, res) => { image.handleImage (req, res, db) })
+
+app.post('/imageurl', (req, res) => { image.handleApiCall (req, res) })
+
+app.listen (process.env.PORT || 3000, () => {
+  console.log (`app is running on port ${process.env.PORT}`);
+})
